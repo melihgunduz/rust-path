cacfde4507f8ccc8e7b60a7c2f8f10fc458789f0
Melih Gündüz <melih.gunduz@hotmail.com>
2026-01-20 23:33:49 +0300

    Lesson-1 Rust cores

--- lesson-1/src/main.rs
+++ lesson-1/src/main.rs
@@ -7,22 +7,37 @@ struct Deck {
     cards: Vec<String>,
 }
 
+// inherent implementations are fancy term of 'add a function to a struct'
+// used to define methods and associated functions
+// associated function: tied to the struct definition (use when you have functionality not tied to a specific instance)
+// method: operates on a specific instance of a sturct (use when you need to read or change fields on a specific instance)
+impl Deck {
+    fn new() -> Self {
+    // Vector's are dynamic sized (can grown/shrink), Array's are fixed sized (can't grow/shrink)
 
+    let suits = ["Hearts", "Spades", "Diamonds"];
+    let values = ["Ace", "Two", "Three"];
+
+    // Bindings are immutable (can't change) by default.
+
+    let mut cards = vec![];
+
+    for suit in suits {
+        for value in values {
+            let card = format!("{} of {}", value, suit);
+            cards.push(card);
+        }
+    }
+
+    // If you delete semicolon from end of the line, rust returns the value automatically, this named as 'implicit return'
+    Deck {cards}    
+    }
+}
 
 
 fn main() {
-    // List of 'suits' - 'hearts', 'spades', 'diamonds'
-    // List of 'values' - 'ace', 'two', 'three'
+    let deck = Deck::new();
 
-    // Double nested for loop
-
-    // Vector's are dynamic sized (can grown/shrink), Array's are fixed sized (can't grow/shrink)
-
-    let suits = vec!["Hearts", "Spades", "Diamonds"];
-    let values = vec!["Ace", "Two", "Three"];
-
-    let deck = Deck {cards: vec![]};
-
-
-    println!("Here's your deck: {:?}", deck);
+    println!("Here's your deck: {:#?}", deck.cards);
+    
 }
