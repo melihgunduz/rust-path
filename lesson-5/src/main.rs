a7c222984302e352576b26b79b8c6c88c52cc562
Melih Gündüz <melih.gunduz@hotmail.com>
2026-02-15 19:20:22 +0300

    Handle errors and results

--- lesson-5/src/main.rs
+++ lesson-5/src/main.rs
@@ -1,15 +1,26 @@
 use std::fs;
 use std::io::Error;
 
+// Many modules in the std lib have their own custom error types
+
 fn main() {
     let text = fs::read_to_string("logs.txt");
 
     // Result enum is used when we need to know if something worked or faied. Ok(value), Err(error)
 
-    if Some((&text)).is_some() {
-        println!("{:#?}", text.unwrap());
-    } else {
-        println!("no data")
+    // if Some((&text)).is_some() {
+    //     println!("{:#?}", text.unwrap());
+    // } else {
+    //     println!("no data")
+    // }
+
+    match divide(5.0, 3.0) {
+        Ok(result_of_division) => {
+            println!("{}", result_of_division)
+        }
+        Err(error_of_division) => {
+            println!("{}", error_of_division)
+        }
     }
     
 }
